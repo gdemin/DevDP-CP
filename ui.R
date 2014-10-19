1991ed2fc556250a6822b31f55c7bc6736202b9b
Gregory Demin <gdemin+github@gmail.com>
2014-10-20 01:45:33 +0400

    Zero commit

---
+++ ui.R
@@ -0,0 +1,18 @@
+shinyUI(pageWithSidebar(
+  headerPanel("Привет"),
+  sidebarPanel(
+    
+    h3("Русский текст")
+    
+    )  ,
+  mainPanel(
+    h3("sdsd"),
+    code("sin"),
+    p("some text")
+    
+    )
+  
+  
+  
+  
+  ))
